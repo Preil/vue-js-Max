3969e20ad2128585eccdeff3aa50e3235a458ca9
preil <preil2000@gmail.com>
2018-05-05 10:30:24 +0300

    Using eventBus for communications between child components.

--- src/components/User.vue
+++ src/components/User.vue
@@ -7,10 +7,13 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :name="name" @nameWasChanged="name = $event"></app-user-detail>
+                <app-user-detail
+                        :name="name"
+                        @nameWasChanged="name = $event"
+                        :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit :userAge="age"></app-user-edit>
             </div>
         </div>
     </div>
@@ -23,7 +26,8 @@
     export default {
         data: function () {
                 return {
-                    name: 'Max'
+                    name: 'Max',
+                    age: 27
                 };
         },
         methods: {
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,18 +3,29 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User name: {{name}}</p>
+        <p>User age: {{userAge}}</p>
         <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+    import {eventBus} from '../main';
+
     export default {
-        props:['name'],
+        props:{
+            name: String,
+            userAge: Number
+        },
         methods: {
             resetName(){
                 this.name = 'Max';
                 this.$emit('nameWasChanged', this.name)
             }
+        },
+        created(){
+            eventBus.$on('ageWasEdited', (age)=>{
+                this.userAge = age;
+            })
         }
     }
 </script>
