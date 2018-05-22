976b58121068d819ade1135b2b615fd27da3f0af
preil <preil2000@gmail.com>
2018-05-22 10:42:01 +0300

    Radio buttons added.

--- src/App.vue
+++ src/App.vue
@@ -57,11 +57,27 @@
                                        value="SendInfomail"
                                        v-model="sendMail"/>Send Infomail</label>
                         </div>
-
-
-
                     </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <div class="form-group">
+                            <label for="male">
+                                <input type="radio"
+                                       id="male"
+                                       value="Male"
+                                       v-model="gender">Male</label>
+
+                            <label for="female">
+                                <input type="radio"
+                                       id="female"
+                                       value="Female"
+                                       v-model="gender">Female</label>
+                        </div>
+                    </div>
+                </div>
+
 
 
                 <hr>
@@ -80,7 +96,7 @@
                                 <ul>
                                     <li v-for="item in sendMail"> {{ item }}</li>
                                 </ul>
-                                <p>Gender: </p>
+                                <p>Gender: {{ gender }} </p>
                                 <p>Priority: </p>
                             </div>
 
@@ -102,7 +118,8 @@
                     age: 37
                 },
                 message: '',
-                sendMail: []
+                sendMail: [],
+                gender:''
             }
         }
     }
