7d34b8701001c246b438af8a0941ad0fe5bfedd2
preil <preil2000@gmail.com>
2018-05-22 10:03:53 +0300

    Textarea added.

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
                         <input type="text"
                                id="email"
                                class="form-control"
-                               v-model="userData.email"> // to bind data in field to variable
+                               v-model="userData.email">  <!--to bind data in field to variable-->
                     </div>
 
                     <div class="form-group">
@@ -33,6 +33,16 @@
                 </div>
                 <div class="row">
                     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <label for="message">Message</label>
+                        <!-- Interpolation between <textarea> {{ test }} </textarea>  doesn't work -->
+                        <textarea id="message"
+                                  rows="5"
+                                  class="form-control"
+                                    v-model="message"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 <h4>Your Data</h4>
@@ -41,7 +51,7 @@
                                 <p>Mail: {{ userData.email }} </p>
                                 <p>Password: {{ userData.password }} </p>
                                 <p>Age: {{ userData.age }}</p>
-                                <p>Message:</p>
+                                <p>Message: {{ message }}</p>
                                 <p><strong>Send Mail?</strong></p>
                                 <ul>
                                     <li></li>
@@ -66,7 +76,8 @@
                     email: '',
                     password: '',
                     age: 37
-                }
+                },
+                message:''
             }
         }
     }
