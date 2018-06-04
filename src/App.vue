85ea7489de7830c07ae09ba2a7f0feb5dcb22259
preil <preil2000@gmail.com>
2018-06-04 17:20:32 +0300

    Slide in-out example added.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
                 <transition name="fade">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
+                <br>
+                <transition name="slide">
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
 
             </div>
         </div>
@@ -39,6 +43,35 @@
     .fade-leave-active {
         transition : 1s;
         opacity: 0;
+    }
+    .slide-enter {
+        
+    }
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+    .slide-leave {
+        
+    }
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
     }
 
 </style>
