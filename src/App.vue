524ce0aa6299ac3135dca75c1b463a974e36f945
preil <preil2000@gmail.com>
2018-06-05 18:40:22 +0300

    Example of transition animation using JS.

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,24 @@
                     <div class="alert alert-info" v-if="show" key="info">This is some info</div>
                     <div class="alert alert-warning" v-else key="warning">This is some warning</div>
                 </transition>
+                <hr>
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br><br>
+
+                <transition
+                        @before-enter="beforeEnter"
+                        @enter="enter"
+                        @after-enter="afterEnter"
+                        @enter-cancelled="enterCancelled"
+
+                        @before-leave="beforeLeave"
+                        @leave="leave"
+                        @after-leave="afterLeave"
+                        @leave-cancelled="leaveCancelled"
+                        :css="false">
+                    <!--we add this to show vue that there no need to find css class for ths element-->
+                    <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
+                </transition>
 
             </div>
         </div>
@@ -35,7 +53,59 @@
     export default {
         data() {
             return {
-                show: true
+                load: true,
+                show: false,
+                elementWidth: 100
+            }
+        },
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter')
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+            },
+            enter(el, done) {
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth + round * 10) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterEnter(el) {
+                console.log('afterEnter')
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled')
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave');
+                this.elementWidth = 300;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el, done) {
+                console.log('leave');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth - round * 10) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterLeave(el) {
+                console.log('afterLeave')
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled')
             }
         }
     }
@@ -46,31 +116,39 @@
     .fade-enter {
         opacity: 0;
     }
+
     .fade-enter-active {
         transition: opacity 1s;
     }
+
     .fade-leave {
         /*opacity: 1; - we comment it because it is "1" by default */
     }
+
     .fade-leave-active {
-        transition : 1s;
+        transition: 1s;
         opacity: 0;
     }
+
     .slide-enter {
-        
+
     }
+
     .slide-enter-active {
         animation: slide-in 1s ease-out forwards;
         transition: opacity 0.5s;
     }
+
     .slide-leave {
-        
+
     }
+
     .slide-leave-active {
         animation: slide-out 1s ease-out forwards;
         transition: opacity 3s;
         opacity: 0;
     }
+
     @keyframes slide-in {
         from {
             transform: translateY(20px);
