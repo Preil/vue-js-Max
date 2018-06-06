fad6aebc220703185916f9be0f1f4403cc42a277
preil <preil2000@gmail.com>
2018-06-06 10:31:00 +0300

    Transition-group animation added.

--- src/App.vue
+++ src/App.vue
@@ -59,13 +59,17 @@
                 <button class="btn btn-primary" @click="addItem">Add Item</button>
                 <br><br>
                 <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="(number, index) in numbers"
-                            @click="removeItem(index)"
-                            style="cursor: pointer">
-                        {{ number }}
-                    </li>
+                    <transition-group name="slide">
+                        <li
+                                class="list-group-item"
+                                v-for="(number, index) in numbers"
+                                @click="removeItem(index)"
+                                style="cursor: pointer"
+                                :key="number">
+                            {{ number }}
+                        </li>
+                    </transition-group>
+
                 </ul>
 
 
@@ -191,8 +195,12 @@
 
     .slide-leave-active {
         animation: slide-out 1s ease-out forwards;
-        transition: opacity 3s;
+        transition: opacity 1s;
         opacity: 0;
+        position: absolute;
+    }
+    .slide-move {
+        transition: transform 1s;
     }
 
     @keyframes slide-in {
