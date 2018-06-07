35d8fb3e0593f8077167866a52c38826bf6e6670
preil <preil2000@gmail.com>
2018-06-07 10:59:30 +0300

    Http send request method added.

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,12 @@
         },
         methods: {
             submit () {
-                console.log(this.user);
+                this.$http.post('https://preil-vue.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                    });
             }
         }
     }
