904b021dc0d2494390e7a450c90c390a32dd9a4a
preil <preil2000@gmail.com>
2018-06-11 21:43:57 +0300

    Using vue-resource.

--- src/App.vue
+++ src/App.vue
@@ -35,31 +35,36 @@
                     email: '',
                     users: []
                 },
-                users:[]
+                users: [],
+                resource: {}
             }
         },
         methods: {
-            submit () {
-                this.$http.post('', this.user)
-                    .then(response => {
-                        console.log(response);
-                    }, error => {
-                        console.log(error);
-                    });
+            submit() {
+//                this.$http.post('data.json', this.user)
+//                    .then(response => {
+//                        console.log(response);
+//                    }, error => {
+//                        console.log(error);
+//                    });
+                this.resource.save({}, this.user)
             },
-            fetchData (){
-                this.$http.get('')
-                    .then (response => {
+            fetchData() {
+                this.$http.get('data.json')
+                    .then(response => {
                         return response.json();
                     })
                     .then(data => {
                         const resultArray = [];
-                        for (let key in data){
+                        for (let key in data) {
                             resultArray.push(data[key]);
                         }
                         this.users = resultArray;
                     })
             }
+        },
+        created () {
+            this.resource = this.$resource('data.json');
         }
     }
 </script>
