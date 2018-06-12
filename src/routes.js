532f9549e139331fcb32b1d8da15e54cbd3ed05e
preil <preil2000@gmail.com>
2018-06-12 20:25:12 +0300

    Mistakes corrected. router-view added to show components accordingly chosen route.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
-
+import User from './components/User.vue';
+import Home from './Home.vue';
 
 export const routes = [
-    {path: '/', component: Home},
+    {path: '', component: Home},
     {path: '/user', component: User},
 
-]
+];
