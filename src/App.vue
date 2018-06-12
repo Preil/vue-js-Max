eb38e676b41da3c8b4ea8ffab4920a41dda95e69
preil <preil2000@gmail.com>
2018-06-12 06:57:29 +0300

    Vue-router empty project.

--- src/App.vue
+++ src/App.vue
@@ -2,80 +2,16 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-                <div class="form-group">
-                    <label>Username</label>
-                    <input type="text" class="form-control" v-model="user.username">
-                </div>
-                <div class="form-group">
-                    <label>Mail</label>
-                    <input type="text" class="form-control" v-model="user.email">
-                </div>
-                <button class="btn btn-primary" @click="submit">Submit</button>
-                <hr>
-                <label>Resource</label>
-                <input type="text" v-model="node">
-
-                <hr>
-                <button class="btn btn-primary" @click="fetchData">Get data</button>
-                <br><br>
-                <ul class="list-group">
-                    <li class="list-group-item" v-for="u in users"> Name: {{ u.username }}  <{{u.email}}> </li>
-                </ul>
+                <h1>Routing</h1>
 
             </div>
-
         </div>
+
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                user: {
-                    username: '',
-                    email: '',
-                    users: []
-                },
-                users: [],
-                resource: {},
-                node: 'data'
-            }
-        },
-        methods: {
-            submit() {
-//                this.$http.post('data.json', this.user)
-//                    .then(response => {
-//                        console.log(response);
-//                    }, error => {
-//                        console.log(error);
-//                    });
-//                this.resource.save({}, this.user)
-                this.resource.saveAlt({node: this.node}, this.user)
-            },
-            fetchData() {
-                this.resource.getData({node: this.node})
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(data => {
-                        const resultArray = [];
-                        for (let key in data) {
-                            resultArray.push(data[key]);
-                        }
-                        this.users = resultArray;
-                    })
-            }
-        },
-        created () {
-            const customActions = {
-                saveAlt: { method: 'POST', url: '{node}.json'},
-                getData: {method: 'GET'}
-            };
-            this.resource = this.$resource('{node}.json', {}, customActions);
-        }
-    }
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
-
-Vue.use(VueResource);
-Vue.http.options.root = 'https://preil-vue.firebaseio.com/';
-Vue.http.interceptors.push((request, next) => {
-    console.log(request);
-    if (request.method == 'POST') {
-        request.method = 'PUT';
-    }
-    next(response => {
-        response.json = () => {
-            return {messages: response.body}
-        }
-    });
-});
 
 new Vue({
     el: '#app',
