5bc41b62b2e1d3a659ab10463716c0593f007d39
preil <preil2000@gmail.com>
2018-06-13 07:30:32 +0300

    GoToHomePage button implemented with router push().

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
         <h1>User component</h1>
+        <p>Some text here</p>
+        <br>
+        <button class="btn btn-primary" @click="goToHomePage">Back to Homepage</button>
     </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            goToHomePage() {
+                this.$router.push('/')
+            }
+        }
+    }
+</script>
