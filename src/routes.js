0ac47e716028ce0828a02298d4fb2ac1b9001640
preil <preil2000@gmail.com>
2018-06-16 12:19:05 +0300

    Multiple Router Views Named. Named Router views.

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,18 @@ import Home from './components/Home.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserDetails from './components/user/UserDetails.vue';
 import UserEdit from './components/user/UserEdit.vue';
+import Header from './components/Header.vue';
+
 
 export const routes = [
-    {path: '', component: Home, name: 'home'},
-    {path: '/user', component: User, children:[
+    {path: '', name: 'home', components: {
+        default: Home, // means Home component for router-view (default - without name)
+        'header-top': Header // means that Header component for router-view named 'header-top'
+    }},
+    {path: '/user', components: {
+        default: User, // means User component for router-view (default - without name)
+        'header-bottom': Header // means that Header component for router-view named 'header-bottom'
+    }, children:[
         {path:'', component: UserStart},
         {path:':id', component: UserDetails},
         {path:':id/edit', component: UserEdit, name: 'userEdit'}
