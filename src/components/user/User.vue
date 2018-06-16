88e00554608d4e5f6b1f2bd1a49a33a54143efa6
preil <preil2000@gmail.com>
2018-06-16 10:33:13 +0300

    Child and nested routes - example beginning.

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,25 +3,14 @@
         <h1>User component</h1>
         <p>Some text here</p>
         <br>
-        <p>Loaded ID: {{ id }}</p>
+
         <button class="btn btn-primary" @click="goToHomePage">Back to Homepage</button>
     </div>
 </template>
 
 <script>
     export default {
-        data () {
-            return {
-                id: this.$route.params.id
-            }
-        },
-        watch: {
-            // watcher is needed to control route changes, because if we stay at the same component,
-            // Vue doesn't reload it.
-            '$route' (to, from) {
-                this.id = to.params.id;
-            }
-        },
+
         methods: {
             goToHomePage() {
                 this.$router.push('/')
