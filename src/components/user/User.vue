33b907035217832591c22b8775b9ae3a854632cf
preil <preil2000@gmail.com>
2018-06-16 09:35:59 +0300

    Dynamic part of route path example.

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,12 +3,18 @@
         <h1>User component</h1>
         <p>Some text here</p>
         <br>
+        <p>Loaded ID: {{ id }}</p>
         <button class="btn btn-primary" @click="goToHomePage">Back to Homepage</button>
     </div>
 </template>
 
 <script>
     export default {
+        data () {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             goToHomePage() {
                 this.$router.push('/')
