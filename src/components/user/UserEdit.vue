2e60c8d35f4028bd3b9a88b21e06d0f127849f47
preil <preil2000@gmail.com>
2018-06-16 19:10:43 +0300

    ScrollBehavior example. Router link with hash selector name.

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -3,5 +3,7 @@
         <h3>User id: {{$route.params.id}} Edit </h3>
         <p>Locale: {{$route.query.locale}}.</p>
         <p>Analytics: {{$route.query.q}}.</p>
+        <div style="height: 800px" ></div>
+        <p id="data">Some tex below.</p>
     </div>
 </template>
