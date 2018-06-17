08b9b9103d0c0444ebbfaec61b8eb2f57f3937d9
preil <preil2000@gmail.com>
2018-06-17 12:27:29 +0300

    Central store usage to get counter value. No need to use props and $events.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
+                <app-result></app-result>
                 <hr>
-                <app-counter @updated="counter += $event"></app-counter>
+                <app-counter></app-counter>
 
             </div>
         </div>
@@ -19,11 +19,6 @@
     import Counter from './components/Counter.vue'
 
     export default {
-        data() {
-            return {
-                counter: 0
-            }
-        },
         components: {
             appResult: Result,
             appCounter: Counter
