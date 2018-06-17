8484a4f1d8917e5974ed20a6e7dbbada5bab8e56
preil <preil2000@gmail.com>
2018-06-17 12:44:26 +0300

    Using getters to access state variables and avoid duplicated code in components.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
                 <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
                 <app-counter></app-counter>
 
@@ -16,11 +17,13 @@
 <script>
 
     import Result from './components/Result.vue'
+    import AnotherResult from './components/AnotherResult.vue'
     import Counter from './components/Counter.vue'
 
     export default {
         components: {
             appResult: Result,
+            appAnotherResult: AnotherResult,
             appCounter: Counter
         }
     }
--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,11 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         counter: 0
+    },
+    getters: { // We use getters to access state variables with some modifications(calculations)
+        // to avoid such repeats inside our application components.
+        doubleCounter: state => {
+            return state.counter * 2;
+        }
     }
 });
