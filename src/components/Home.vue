697bee98ded663db4d773f6b2daf8193ab99f496
preil <preil2000@gmail.com>
2018-06-17 11:26:05 +0300

    VUEX. New project central store example.

--- src/components/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Home component</h1>
-    </div>
-</template>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
-import {routes} from './routes'
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-    routes,
-    mode: 'history',
-    scrollBehavior (to, from, savePosition) {
-        if(to.hash){
-            return {selector: to.hash}
-        }
-    }
-});
-
-router.beforeEach((to, from, next)=>{
-    console.log('global beforeEach');
-    next();
-    // next(false); - to deny routing
-    // next('/'); - go to path '/'
-    // next({ path: 'home'}); - go to named path
-})
 
 new Vue({
     el: '#app',
-    router,
     render: h => h(App)
 });
