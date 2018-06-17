ce831eed02cfd5a44506b2c030846d6c306602e1
preil <preil2000@gmail.com>
2018-06-17 08:45:20 +0300

    Protecting route inside component, using beforeRouteEnter example.

--- src/components/user/UserDetails.vue
+++ src/components/user/UserDetails.vue
@@ -16,6 +16,19 @@
             return {
                 id: this.$route.params.id
             }
+        },
+        beforeRouteEnter (to, from, next) {
+            if (true) { // Can check here User authentication?
+                console.log('beforeRouteEnter')
+                next();
+            } else {
+                next(false);
+            }
+//            this.id - doesn't work, to access id we use next construction;
+//            next(vm =>{
+//                vm.id;
+//            })  - this construction should be used if we needed to access data from this route component.
+//            Because Vue doesn't create component at all, if next doesn't get true.
         }
     }
 </script>
