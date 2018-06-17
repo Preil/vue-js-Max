f1d2d1d2801a8e8ca9ca90e27c9f365d4c4a74a3
preil <preil2000@gmail.com>
2018-06-17 10:38:13 +0300

    Lazy loading. Grouping components in a bundles.

--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,22 @@ import Home from './components/Home.vue';
 const User = resolve => {
     require.ensure(['./components/user/User.vue'], () => {
         resolve(require('./components/user/User.vue'));
-    });
+    }, 'user');
 };
 const UserStart = resolve => {
     require.ensure(['./components/user/UserStart.vue'], () => {
         resolve(require('./components/user/UserStart.vue'));
-    });
+    }, 'user');
 };
 const UserDetails = resolve => {
     require.ensure(['./components/user/UserDetails.vue'], () => {
         resolve(require('./components/user/UserDetails.vue'));
-    });
+    }, 'user');
 };
 const UserEdit = resolve => {
     require.ensure(['./components/user/UserEdit.vue'], () => {
         resolve(require('./components/user/UserEdit.vue'));
-    });
+    }, 'user');
 };
 
 
