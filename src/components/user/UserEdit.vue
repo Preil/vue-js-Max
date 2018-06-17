09773c5587e37abe914a46506712ccd084360a88
preil <preil2000@gmail.com>
2018-06-17 09:27:11 +0300

    Protecting leave route component, using beforeRouteLeave example.

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -3,7 +3,32 @@
         <h3>User id: {{$route.params.id}} Edit </h3>
         <p>Locale: {{$route.query.locale}}.</p>
         <p>Analytics: {{$route.query.q}}.</p>
-        <div style="height: 800px" ></div>
+        <hr>
+        <button class="btn btn-primary" @click="confirmed=true">Confirm</button>
+        <h3 v-if="confirmed">Confirmed!!!</h3>
+        <div style="height: 200px"></div>
         <p id="data">Some tex below.</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave (to, from, next) {
+            if(this.confirmed){
+                next();
+            } else {
+                if (confirm('Are you sure?')){
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+        }
+    }
+
+</script>
