8db659fa26fdb30c96a56ad53d54d48176f73f8b
preil <preil2000@gmail.com>
2018-08-29 12:48:51 +0300

    New project - Stock trader started.

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,11 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Vuex</h1>
-                <app-result></app-result>
-                <app-another-result></app-another-result>
-                <hr>
-                <app-counter></app-counter>
-                <br>
-                <p>Another Counter:</p>
-                <app-another-counter></app-another-counter>
 
-                <br>
-                <hr>
-                <input type="text" v-model="value">
-                <p>{{value}}</p>
-
-            </div>
-        </div>
-
-    </div>
 </template>
 
+
 <script>
+    export default {}
 
-    import Result from './components/Result.vue';
-    import AnotherResult from './components/AnotherResult.vue';
-    import Counter from './components/Counter.vue';
-    import AnotherCounter from './components/AnotherCounter.vue';
-
-    export default {
-        computed: {
-            value: {
-                get() {
-                    return this.$store.getters.value;
-                },
-                set(value) {
-                    this.$store.dispatch('updateValue', value);
-                }
-            }
-        },
-        components: {
-            appResult: Result,
-            appAnotherResult: AnotherResult,
-            appCounter: Counter,
-            appAnotherCounter: AnotherCounter
-
-        }
-    }
 </script>
 
 <style>
